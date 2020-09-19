a4fcc142e8a08749d47e7644d07c410985ea397e
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-09-20 08:22:42 +0900

    fix toBe

--- tools/src/testing.ts
+++ tools/src/testing.ts
@@ -2,6 +2,7 @@ type TestingParts = {
   toBeTrue: string
   toBeFalse: string
   toBeUndefined: string
+  toBe: string
   toEqual: string
   toBeInstanceOf: string
   beforeAll: string
@@ -19,6 +20,7 @@ export const testing: Testing = {
     toBeTrue: 'toBe(true)',
     toBeFalse: 'toBe(false)',
     toBeUndefined: 'toBeUndefined()',
+    toBe: 'toBe',
     toEqual: 'toEqual',
     beforeAll: 'beforeAll',
     afterAll: 'afterAll',
@@ -29,6 +31,7 @@ export const testing: Testing = {
     toBeTrue: 'to.be.true',
     toBeFalse: 'to.be.false',
     toBeUndefined: 'to.be.undefined',
+    toBe: 'to.equal',
     toEqual: 'to.deep.equal',
     toBeInstanceOf: 'to.be.an.instanceof',
     beforeAll: 'before',
